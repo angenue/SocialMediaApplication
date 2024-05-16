8c15ff4d4130a858e0bb3d27895b0c98e1a70a91
angenue <angelica1nuesi@gmail.com>
2024-05-15 21:27:54 -0400

    added api calls to retrive likes and comments made by user

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.CommentDto;
+import com.example.backend.dto.PostDto;
 import com.example.backend.dto.UserDto;
 import com.example.backend.service.UserService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController // This means that this class is a Controller
 @RequestMapping(path="/user") // This means URL's start with /user (after Application path)
@@ -27,6 +31,16 @@ public class UserController {
     @PostMapping("/{followerId}/follow/{followedId}")
     public void followUser(@PathVariable Long followerId, @PathVariable Long followedId) {
         userService.followUser(followerId, followedId);
+    }
+
+    @GetMapping("/{userId}/likedPosts")
+    public List<PostDto> getLikedPostsByUser(@PathVariable Long userId) {
+        return userService.getLikedPostsByUser(userId);
+    }
+
+    @GetMapping("/{userId}/comments")
+    public List<CommentDto> getCommentsByUser(@PathVariable Long userId) {
+        return userService.getCommentsByUser(userId);
     }
 }
 
