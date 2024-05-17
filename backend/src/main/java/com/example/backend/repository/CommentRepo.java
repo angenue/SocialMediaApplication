d687e66750c2c1ddf2c4d3d602d10b0655039b77
angenue <angelica1nuesi@gmail.com>
2024-05-16 23:40:30 -0400

    queries

--- backend/src/main/java/com/example/backend/repository/CommentRepo.java
+++ backend/src/main/java/com/example/backend/repository/CommentRepo.java
@@ -5,6 +5,11 @@ import com.example.backend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
+
 @Repository
 public interface CommentRepo extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByPostPostId(Long postId);
+    List<Comment> findAllByParentComment(Comment parentComment);
 }
