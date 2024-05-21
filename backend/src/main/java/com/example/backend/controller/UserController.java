3d0f3adb93f4d40a75fab31845e288e59e98d7c7
angenue <angelica1nuesi@gmail.com>
2024-05-21 15:19:01 -0400

    controller api methods

--- backend/src/main/java/com/example/backend/controller/UserController.java
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -4,12 +4,15 @@ import com.example.backend.dto.CommentDto;
 import com.example.backend.dto.PostDto;
 import com.example.backend.dto.UserDto;
 import com.example.backend.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController // This means that this class is a Controller
-@RequestMapping(path="/user") // This means URL's start with /user (after Application path)
+@RequestMapping("/user") // This means URL's start with /user (after Application path)
 
 public class UserController {
     private final UserService userService;
@@ -18,14 +21,40 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/{username}")
-    public UserDto getUserByUsername(@PathVariable String username) {
-        return userService.getUserByUsername(username);
+    @PostMapping("/register")
+    public ResponseEntity<Void> createUser(@Valid @RequestBody UserDto userDto) {
+        userService.createUser(userDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PostMapping("/register")
-    public void createUser(@RequestBody UserDto userDto) {
-        userService.createUser(userDto);
+   /* @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long userId) {
+        UserDto userDto = userService.getUserById(userId);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }*/
+
+    @GetMapping("/{username}")
+    public ResponseEntity<UserDto> getUserByUsername(@PathVariable String username) {
+        UserDto userDto = userService.getUserByUsername(username);
+        return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/settings/{userId}")
+    public ResponseEntity<Void> updateProfile(@Valid @RequestBody UserDto userDto) {
+        userService.updateProfile(userDto);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/{userId}/password")
+    public ResponseEntity<Void> updatePassword(@PathVariable Long userId, @Valid @RequestBody UserDto userDto) {
+        userService.updatePassword(userId, userDto.getCurrentPassword(), userDto.getNewPassword(), userDto.getNewPasswordConfirmation());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+        userService.deleteUser(userId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/{followerId}/follow/{followedId}")
