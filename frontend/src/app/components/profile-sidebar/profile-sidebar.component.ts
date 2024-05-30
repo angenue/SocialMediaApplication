99b539fe11ea44c1356567f83d3c7b9d4517f3f7
angenue <angelica1nuesi@gmail.com>
2024-05-30 00:48:05 -0400

    implement components for each feature

---
+++ frontend/src/app/components/profile-sidebar/profile-sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-sidebar.component.html',
+  styleUrl: './profile-sidebar.component.scss'
+})
+export class ProfileSidebarComponent {
+
+}
--- frontend/src/app/components/user-profile.component.ts
+++
@@ -1,7 +0,0 @@
-// src/app/users/user-profile.component.ts
-
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../services/user.service';
-import { User } from '../models/user.model';
-
-
