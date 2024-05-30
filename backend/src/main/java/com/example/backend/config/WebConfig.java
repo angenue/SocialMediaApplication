7116c38249f8e7d19fc59b3ed4f6e59122a35364
angenue <angelica1nuesi@gmail.com>
2024-05-30 00:02:59 -0400

    updated port to match angulars port

--- backend/src/main/java/com/example/backend/config/WebConfig.java
+++ backend/src/main/java/com/example/backend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")  // URL of the frontend
+                .allowedOrigins("http://localhost:4200")  // URL of the frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
